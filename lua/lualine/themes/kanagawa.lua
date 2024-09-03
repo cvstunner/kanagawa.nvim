02fc652e34b7f802dea5208f0178e2659af38dfc
Chetan Vithoba Shigvan <chetanshigwan789@gmail.com>
2024-09-03 15:55:09 +0530

    Update kanagawa.lua

--- lua/lualine/themes/kanagawa.lua
+++ lua/lualine/themes/kanagawa.lua
@@ -1,44 +1,43 @@
-local colors = require("kanagawa.colors").setup()
-local theme = colors.theme
+local theme = require("kanagawa.colors").setup().theme
 
 local kanagawa = {}
 
 kanagawa.normal = {
-	a = { bg = theme.modes.normal, fg = theme.ui.bg_m3 },
-	b = { bg = theme.ui.bg_p2, fg = theme.modes.normal },
-	c = { bg = theme.ui.bg_p1, fg = theme.ui.fg_gray },
+  a = { bg = theme.vcs.changed, fg = theme.ui.bg_m3 },
+  b = { bg = theme.diff.change, fg = theme.syn.fun },
+  c = { bg = theme.ui.bg_p1, fg = theme.ui.fg },
 }
 
 kanagawa.insert = {
-	a = { bg = theme.modes.insert, fg = theme.ui.bg },
-	b = { bg = theme.ui.bg_p2, fg = theme.modes.insert },
+  a = { bg = theme.diag.ok, fg = theme.ui.bg },
+  b = { bg = theme.ui.bg, fg = theme.diag.ok },
 }
 
 kanagawa.command = {
-	a = { bg = theme.modes.command, fg = theme.ui.bg },
-	b = { bg = theme.ui.bg_p2, fg = theme.modes.command },
+  a = { bg = theme.syn.operator, fg = theme.ui.bg },
+  b = { bg = theme.ui.bg, fg = theme.syn.operator },
 }
 
 kanagawa.visual = {
-	a = { bg = theme.modes.visual, fg = theme.ui.bg },
-	b = { bg = theme.ui.bg_p2, fg = theme.modes.visual },
+  a = { bg = theme.syn.keyword, fg = theme.ui.bg },
+  b = { bg = theme.ui.bg, fg = theme.syn.keyword },
 }
 
 kanagawa.replace = {
-	a = { bg = theme.modes.replace, fg = theme.ui.bg },
-	b = { bg = theme.ui.bg_p2, fg = theme.modes.replace },
+  a = { bg = theme.syn.constant, fg = theme.ui.bg },
+  b = { bg = theme.ui.bg, fg = theme.syn.constant },
 }
 
 kanagawa.inactive = {
-	a = { bg = theme.ui.bg_m3, fg = theme.ui.fg_gray },
-	b = { bg = theme.ui.bg_m3, fg = theme.ui.fg_gray, gui = "bold" },
-	c = { bg = theme.ui.bg_m3, fg = theme.ui.fg_gray },
+  a = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+  b = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim, gui = "bold" },
+  c = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
 }
 
 if vim.g.kanagawa_lualine_bold then
-	for _, mode in pairs(kanagawa) do
-		mode.a.gui = "bold"
-	end
+  for _, mode in pairs(kanagawa) do
+    mode.a.gui = "bold"
+  end
 end
 
 return kanagawa
